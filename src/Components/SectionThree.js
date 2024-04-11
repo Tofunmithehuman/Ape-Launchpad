4c3a24b9178882adbd8eb3be28bba2153f1a9d1d
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-04-11 13:57:54 +0100

    seven  commit

---
+++ src/Components/SectionThree.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Styles/style.css";
+
+function SectionThree() {
+  return (
+    <div className='SectionT'>SectionThree</div>
+  )
+}
+
+export default SectionThree
